014d3b8e6609d8f03b8f1f45c5a4b3e44ef1bf6c
Samantha <samanthabarrueta@gmail.com>
2020-07-22 20:01:47 -0500

    get and post roles and employees

--- routes.js
+++ routes.js
@@ -15,6 +15,26 @@ exports.route = (app, connection) => {
 
     app.post("/departments", async (request, response) => {
         const [data] = await connection.query(`INSERT INTO department SET ?`, request.body);
-        response.json({created: true, customer_id: data.insertId, ...request.body});
+        response.json({...request.body});
     });
+
+    app.get("/role", async (request, response) => {
+        const [data] = await connection.query(`SELECT * FROM role`);
+        response.json(data);
+    });
+
+    app.post("/role", async (request, response) => {
+        const [data] = await connection.query(`INSERT INTO role SET ?`, request.body);
+        response.json({...request.body});
+    });
+
+    app.get("/employee", async (request, response) => {
+        const [data] = await connection.query(`SELECT * FROM employee`);
+        response.json(data);
+    });
+
+    app.post("/employee", async (request, response) => {
+        const [data] = await connection.query(`INSERT INTO employee SET ?`, request.body);
+        response.json({...request.body});
+    });    
 }
